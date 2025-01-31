80e49db32a0f551cf6e10c341ad36cff06d32738
nyradhr <alessio.piccione@hotmail.it>
2025-01-31 21:46:50 +0100

    drawing cells

--- src/main.py
+++ src/main.py
@@ -1,13 +1,20 @@
 from window import Window
 from geometry import Point, Line
+from cell import Cell
 
 def main():
     win = Window(800, 600)
-    #drawing some lines to test newly created Point and Line classes
-    line_1 = Line(Point(0,0), Point(30,60))
-    line_2 = Line(Point(50,0), Point(50,100))
-    win.draw_line(line_1, "blue")
-    win.draw_line(line_2, "red")
+    #add some cells and draw them
+    cell_1 = Cell(win)
+    cell_1.has_bottom_wall = False
+    cell_1.draw(30, 30, 70, 70)
+    cell_2 = Cell(win)
+    cell_2.has_left_wall = False
+    cell_2.draw(55, 55, 130, 130)
+    cell_3 = Cell(win)
+    cell_3.has_top_wall = False
+    cell_3.draw(350, 350, 600, 600)
+
     win.wait_for_close()
 
 main()
