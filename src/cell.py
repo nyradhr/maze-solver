4dd437c744afacecf69280f213df514f3b493e37
nyradhr <alessio.piccione@hotmail.it>
2025-01-31 22:02:01 +0100

    drawing coloured lines that connect cells

--- src/cell.py
+++ src/cell.py
@@ -30,3 +30,13 @@ class Cell:
         if self.has_bottom_wall:
             line = Line(Point(x1, y2), Point(x2, y2))
             self._win.draw_line(line, "blue")
+
+    def draw_move(self, to_cell, undo=False):
+        if not undo:
+            color = "red"
+        else:
+            color = "gray"
+        from_center = Point((self._x1+self._x2)/2, (self._y1+self._y2)/2)
+        to_center = Point((to_cell._x1+to_cell._x2)/2, (to_cell._y1+to_cell._y2)/2)
+        line = Line(from_center, to_center)
+        self._win.draw_line(line, color)
--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from window import Window
-from geometry import Point, Line
+#from geometry import Point, Line
 from cell import Cell
 
 def main():
@@ -10,10 +10,18 @@ def main():
     cell_1.draw(30, 30, 70, 70)
     cell_2 = Cell(win)
     cell_2.has_left_wall = False
-    cell_2.draw(55, 55, 130, 130)
+    cell_2.draw(155, 155, 230, 230)
     cell_3 = Cell(win)
     cell_3.has_top_wall = False
-    cell_3.draw(350, 350, 600, 600)
+    cell_3.draw(150, 350, 400, 600)
+
+    cell_4 = Cell(win)
+    cell_4.draw(350, 200, 650, 500)
+
+    #let's try drawing a line from a cell to another
+    cell_1.draw_move(cell_3, True) 
+
+    cell_2.draw_move(cell_4) #undo=False as default
 
     win.wait_for_close()
 
